ded273116ba3f645d2ab66f25e81266de64111ef
Valentin Jimenez <santi.job.2022@gmail.com>
2023-12-01 20:49:01 -0600

    Adding useState and useEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { useState, useEffect} from "react";
 import CardList from "../components/CardList";
 //import {robots} from './robots'; //you can import multiple objects from './robots' because it has export instead of export default
 import SearchBox from "../components/SearchBox";
@@ -6,49 +6,95 @@ import './App.css';
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends Component{
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots:users}));
-    }
+    const [robots, setRobots] = useState([]);
+    const [searchfield,setSearchfield] = useState("");
+    const [count, setCount] = useState(0);
 
-    onSearchChange = (event)=>{ // here we used the arrow notation to make this local instead of belonging to "input" in SearchBox
-        this.setState({searchfield: event.target.value})        
-    }
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((data) => setRobots(data));               
+        console.log(count);
+    }, [count]);
 
-    render (){
-        const filteredRobots = this.state.robots.filter((robot) => {
-            return robot.name
-            .toLowerCase()
-            .includes(this.state.searchfield.toLowerCase());
-        });
-        if(this.state.robots.length === 0){
-            return <h1>Loading</h1>
-        }
-        else{        
-            return (
-              <div className="tc">
-                <h1 className="f2">Robofriends</h1>
+    const onSearchChange = (event) => {
+        // here we used the arrow notation to make this local instead of belonging to "input" in SearchBox
+        setSearchfield( event.target.value );
+    };  
+        
+    const filteredRobots = robots.filter((robot) =>
+        robot.name.toLowerCase().includes(searchfield.toLowerCase())
+      );    
 
-                <SearchBox searchChange={this.onSearchChange} />
-                <Scroll>
-                  <ErrorBoundry>
-                    <CardList robots={filteredRobots} />
-                  </ErrorBoundry>
-                </Scroll>
-              </div>
-            );
-        }
+    if (robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">Robofriends</h1>
+            <button onClick={()=>setCount(count+1)}>Click Me!</button>
+          <SearchBox searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
     }
+  
 }
 
 export default App;
+
+
+
+
+// class App extends Component{
+//     constructor(){
+//         super()
+//         this.state = {
+//             robots: [],
+//             searchfield: ''
+//         }
+//     }
+
+//     componentDidMount(){
+//         fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(response => response.json())
+//         .then(users => this.setState({robots:users}));
+//     }
+
+//     onSearchChange = (event)=>{ // here we used the arrow notation to make this local instead of belonging to "input" in SearchBox
+//         this.setState({searchfield: event.target.value})        
+//     }
+
+//     render (){
+//         const filteredRobots = this.state.robots.filter((robot) => {
+//             return robot.name
+//             .toLowerCase()
+//             .includes(this.state.searchfield.toLowerCase());
+//         });
+//         if(this.state.robots.length === 0){
+//             return <h1>Loading</h1>
+//         }
+//         else{        
+//             return (
+//               <div className="tc">
+//                 <h1 className="f2">Robofriends</h1>
+
+//                 <SearchBox searchChange={this.onSearchChange} />
+//                 <Scroll>
+//                   <ErrorBoundry>
+//                     <CardList robots={filteredRobots} />
+//                   </ErrorBoundry>
+//                 </Scroll>
+//               </div>
+//             );
+//         }
+//     }
+// }
+
+// export default App;
